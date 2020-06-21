e57ea60cbb91fc335e19316d32b4d8dcd4100aac
MYQ12 <yehyaqureshi@gmail.com>
2020-06-21 03:56:53 -0400

    Update TMA3Question3.h

--- TMA3/TMA3Question3.h
+++ TMA3/TMA3Question3.h
@@ -5,34 +5,34 @@
 #include <cmath>
 
 class Point{
-	private:
-		double x, y;
-	
-	public:
-		Point(){   
-			x = 0; 
-			y = 0;   
-		}
+private:
+	double x, y;
 
-    Point(double px, double py){   
+public:
+	Point(){   
+		x = 0; 
+		y = 0;   
+	}
+
+	Point(double px, double py){   
 	    x = px;  
 	    y = py;
 	  }
 
-    friend std::ostream & operator << (std::ostream &os, const Point &p);
-    Point &operator+(const Point&rhs);
-    Point &operator-(const Point&rhs);
+	friend std::ostream & operator << (std::ostream &os, const Point &p);
+	Point &operator+(const Point&rhs);
+	Point &operator-(const Point&rhs);
 
-    void setPoint(double px, double py){   
+	void setPoint(double px, double py){   
 	    x = px; 
 	    y = py; 
 	  }
 
-    double get_x() const{  
+	double get_x() const{  
 	     return x;   
 	  }
 
-    double get_y() const{  
+	double get_y() const{  
 	    return y;   
 	  }
 };
@@ -45,41 +45,41 @@ struct enclosure{
 };
 
 class Shape{	
-  public:
-	  virtual double area() const = 0;
-    virtual double perimeter () const = 0;
-    virtual void display() = 0;
-  
-	protected:
-		enclosure bounding_box;		
+public:
+	virtual double area() const = 0;
+	virtual double perimeter () const = 0;
+	virtual void display() = 0;
+	
+protected:
+	enclosure bounding_box;		
 };
 
 class Circle: public Shape{
-	private:
-		double radius;
+private:
+	double radius;
 		
-	public:
-		Circle(); 
-		
-		Circle(const double radius, const Point center);
+public:
+	Circle(); 
 
-    double perimeter() const{   
+	Circle(const double radius, const Point center);
+
+	double perimeter() const{   
 	    return 2 * radius * M_PI;
-    }
-    
-    double area() const{   
+	}
+
+	double area() const{   
 	    return M_PI *(radius * radius);
-    }
-    
-    void display(){
-	    std::cout << "Class: Circle"	<< std::endl;
-			std::cout << "Area: " << area() << " units squared" << std::endl;
-			std::cout << "Circumference: " << perimeter() << " units" << std::endl;
-			std::cout << "Bounding Box: " << "(" << bounding_box.top_right.get_x() << ", " << bounding_box.top_right.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.bottom_right.get_x() << ", " << bounding_box.bottom_right.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.top_left.get_x() << ", " << bounding_box.top_left.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.bottom_left.get_x() << ", " << bounding_box.bottom_left.get_y() << ")" << "\n\n";
- 		}
+	}
+
+	void display(){
+		std::cout << "Class: Circle"	<< std::endl;
+		std::cout << "Area: " << area() << " units squared" << std::endl;
+		std::cout << "Circumference: " << perimeter() << " units" << std::endl;
+		std::cout << "Bounding Box: " << "(" << bounding_box.top_right.get_x() << ", " << bounding_box.top_right.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.bottom_right.get_x() << ", " << bounding_box.bottom_right.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.top_left.get_x() << ", " << bounding_box.top_left.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.bottom_left.get_x() << ", " << bounding_box.bottom_left.get_y() << ")" << "\n\n";
+	}
 };
 
 Circle::Circle(const double radius, const Point center):radius(radius){
@@ -90,31 +90,31 @@ Circle::Circle(const double radius, const Point center):radius(radius){
 }
 
 class Square:public Shape{
-	private:
-		double side;
-	
-	public:
-		Square(); 
+private:
+	double side;
+
+public:
+	Square(); 
+
+	Square(const double side, const Point center);
+
+	double area()const{
+		return side * side;
+	}
+
+	double perimeter()const{
+		return 4 * side;
+	}
 		
-		Square(const double side, const Point center);
-		
-		double area()const{
-			return side * side;
-		}
-		
-		double perimeter()const{
-			return 4 * side;
-		}
-		
-		void display(){
-	    std::cout << "Class: Square"	<< std::endl;
-			std::cout << "Area: " << area() << " units squared" << std::endl;
-			std::cout << "Perimeter: " << perimeter() << " units" << std::endl;
-			std::cout << "Bounding Box: " << "(" << bounding_box.top_right.get_x() << ", " << bounding_box.top_right.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.bottom_right.get_x() << ", " << bounding_box.bottom_right.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.top_left.get_x() << ", " << bounding_box.top_left.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.bottom_left.get_x() << ", " << bounding_box.bottom_left.get_y() << ")" << "\n\n";
-    }
+	void display(){
+		std::cout << "Class: Square"	<< std::endl;
+		std::cout << "Area: " << area() << " units squared" << std::endl;
+		std::cout << "Perimeter: " << perimeter() << " units" << std::endl;
+		std::cout << "Bounding Box: " << "(" << bounding_box.top_right.get_x() << ", " << bounding_box.top_right.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.bottom_right.get_x() << ", " << bounding_box.bottom_right.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.top_left.get_x() << ", " << bounding_box.top_left.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.bottom_left.get_x() << ", " << bounding_box.bottom_left.get_y() << ")" << "\n\n";
+	}
 };
 
 Square::Square(const double side, const Point center):side(side){	
@@ -125,71 +125,72 @@ Square::Square(const double side, const Point center):side(side){
 }
 
 class Triangle: public Shape{
-	private:
-		double side1;
-		double side2;
-		double side3;
-	
-	public:
-		Triangle();
+private:
+	double side1;
+	double side2;
+	double side3;
+
+public:
+	Triangle();
+
+	Triangle(const double side1, const double side2, const double side3, const Point center);
+
+	double area()const{
+		double half_perimeter = (side1 + side2 + side3)/2;
+
+		return pow(half_perimeter*(half_perimeter - side1)*(half_perimeter - side2)*(half_perimeter - side3), 0.5);
+	}
+
+	double perimeter()const{
+		return side1 + side2 + side3;
+	}
+
+	double max (double a, double b, double c){
+		double max_value = 0;
+
+		if( a >= b && a >= c)	
+			max_value = a;
+
+		else if( b >= a && b >= c)	
+			max_value = b;
+
+		else	
+			max_value = c;
+
+		return max_value;
+	}
 		
-		Triangle(const double side1, const double side2, const double side3, const Point center);
+	double min (double aO, double bO, double cO){
+		double min_value = 0;
+
+		if( aO <= bO && aO <= cO)	
+			min_value = aO;
+
+		else if( bO <= aO && bO <= cO)	
+			min_value = bO;
+
+		else	
+			min_value = cO;
+
+		return min_value;
+	}
 		
-		double area()const{
-			double half_perimeter = (side1 + side2 + side3)/2;
-			 
-			return pow(half_perimeter*(half_perimeter - side1)*(half_perimeter - side2)*(half_perimeter - side3), 0.5);
-		}
+	void display(){
+		std::cout << "Class: Triangle"	<< std::endl;
+
+		if(area() > 0){
+			std::cout << "Area: " << area() << " units squared" << std::endl;
+			std::cout << "Perimeter: " << perimeter() << " units" << std::endl;
+		}	
 		
-		double perimeter()const{
-			return side1 + side2 + side3;
-		}
-		
-		double max (double a, double b, double c){
-			double max_value = 0;
-			
-			if( a >= b && a >= c)	
-				max_value = a;
-			
-			else if( b >= a && b >= c)	
-				max_value = b;
-			
-			else	
-				max_value = c;
-			
-			return max_value;
-		}
-		
-		double min (double aO, double bO, double cO){
-			double min_value = 0;
-			
-			if( aO <= bO && aO <= cO)	
-				min_value = aO;
-			
-			else if( bO <= aO && bO <= cO)	
-				min_value = bO;
-			
-			else	
-				min_value = cO;
-			
-			return min_value;
-		}
-		
-		void display(){
-	    std::cout << "Class: Triangle"	<< std::endl;
-	    
-	    if(area() > 0){
-				std::cout << "Area: " << area() << " units squared" << std::endl;
-				std::cout << "Perimeter: " << perimeter() << " units" << std::endl;
-			}	
-			else
-				std::cout << "The inputted sides do not form a triangle!" << std::endl;
-		
-			std::cout << "Bounding Box: " << "(" << bounding_box.top_right.get_x() << ", " << bounding_box.top_right.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.bottom_right.get_x() << ", " << bounding_box.bottom_right.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.top_left.get_x() << ", " << bounding_box.top_left.get_y() << ")";
-			std::cout << " " << "(" << bounding_box.bottom_left.get_x() << ", " << bounding_box.bottom_left.get_y() << ")" << "\n\n";
-    }
+		else
+			std::cout << "The inputted sides do not form a triangle!" << std::endl;
+
+		std::cout << "Bounding Box: " << "(" << bounding_box.top_right.get_x() << ", " << bounding_box.top_right.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.bottom_right.get_x() << ", " << bounding_box.bottom_right.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.top_left.get_x() << ", " << bounding_box.top_left.get_y() << ")";
+		std::cout << " " << "(" << bounding_box.bottom_left.get_x() << ", " << bounding_box.bottom_left.get_y() << ")" << "\n\n";
+	}
 };
 
 Triangle::Triangle(const double side1, const double side2, const double side3, const Point ref_point):side1(side1), side2(side2), side3(side3){
