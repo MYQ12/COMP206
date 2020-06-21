c8beb61d80817cbe57a38b5e9d8a178f5c6e941f
MYQ12 <yehyaqureshi@gmail.com>
2020-06-21 03:39:45 -0400

    Add files via upload

---
+++ TMA1/TMA1Question2.cpp
@@ -0,0 +1,99 @@
+/* 
+ Title: TMA1Question2.cpp
+ Author: Mohammad Yehya Qureshi
+*/
+
+/*
+ DOCUMENTATION
+ 
+ Program Purpose: Open a text file, and display the contents of that file one line at a time. After a line has been displayed, 
+ 				  the program should wait for the user to press a key before displaying the next line in the file.
+
+ Compile: c++ -o TMA1Question2.exe TMA1Question2.cpp
+ Execution: ./TMA1Question2.exe
+ 
+ Classes: none
+
+ Variables:
+ 	fin - ifstream - used to open the file for reading
+ 	lines - string - used to store the contents of the file one line at a time
+*/
+
+/*
+ TEST PLAN
+ 
+ Test case 1: Valid data run
+ 	Input:	Hello my name is Mohammad Yehya Qureshi.
+ 			I am 18 years old.
+ 			I live in Brampton Ontario.
+ 			
+ 	Output: Hello my name is Mohammad Yehya Qureshi.
+			Press the ENTER key to continue
+
+			I am 18 years old.
+			Press the ENTER key to continue
+
+			I live in Brampton Ontario.
+			Press the ENTER key to continue
+
+ 
+ Test case 2: Valid data run
+ 	Input: Hello my name is Mohammad Yehya Qureshi. I am 18 years old.I live in Brampton Ontario.
+ 	Output: Hello my name is Mohammad Yehya Qureshi. I am 18 years old.I live in Brampton Ontario.
+			Press the ENTER key to continue
+				
+ Test case 3: Valid data run
+ 	Input: 1
+	 		   2
+	 		   3
+	 		   4
+	 		   5
+ 		    
+ 	Output: 1
+					Press the ENTER key to continue
+		
+					2
+					Press the ENTER key to continue
+		
+					3
+					Press the ENTER key to continue
+		
+					4
+					Press the ENTER key to continue
+		
+					5
+					Press the ENTER key to continue
+
+ 
+ Test case 4: Abnormal data run
+ 	Input: File not found 
+ 	Output: $
+ 
+ Test case 5: Limit conditions run
+ 	Input: File is empty
+ 	Output: $
+ 		
+ Discussion:
+ 	The program accepts single words, numbers, or integers to full sentences.
+ 	The program will default to a blank console if the the file cannot be found or is empty.
+ 	The program only works if you press the ENTER KEY.
+ 
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream fin("TMA1Q2.txt");
+    string lines;
+    
+   while (getline(fin, lines)){
+        cout << lines << endl; 
+        cout << "Press the ENTER key to continue" << endl;
+        cin.get();
+    }
+    
+    fin.close();   
+} ///:~
---
+++ TMA1/TMA1Question3.cpp
@@ -0,0 +1,73 @@
+/* 
+ Title: TMA1Question3.cpp
+ Author: Mohammad Yehya Qureshi
+*/
+
+/*
+ DOCUMENTATION
+ 
+ Program Purpose: Use two nested for loops and the modulus operator to detect and print the prime numbers from 1 to 10,000.
+
+ Compile: c++ -o TMA1Question3.exe TMA1Question3.cpp
+ Execution: ./TMA1Question3.exe
+ 
+ Classes: none
+
+ Variables: chk - bool - used to represent whether the value in the outer loop is a prime number or not
+*/
+
+/*
+ TEST PLAN
+ 
+ Test case 1: Valid data run
+ 	Input: for (int i = 2; i < 10000; i++)
+ 	Output: 2 is Prime, 3 is Prime, 5 is Prime, 7 is Prime, 11 is Prime, .... , 9973 is Prime
+ 	
+ Test case 2: Valid data run
+ 	Input: for (int i = 3; i < 10000; i++)
+ 	Output: 3 is Prime, 5 is Prime, 7 is Prime, 11 is Prime, .... , 9973 is Prime
+ 	
+ Test case 3: Valid data run
+ 	Input: for (int i = 4; i < 10000; i++)
+ 	Output: 5 is Prime, 7 is Prime, 11 is Prime, .... , 9973 is Prime
+ 	
+ Test case 4: Abnormal data run
+ 	Input: for (int i = -2; i < 10000; i++) 
+ 	Output: error: -2 is Prime, -1 is Prime, 0 is Prime, 1 is Prime, 2 is Prime, 3 is Prime, 5 is Prime, 7 is Prime, 11 is Prime ... 9973 is Prime
+
+ Test case 5`: Limit conditions run
+ 	Input: for (int i = 0; i < 10000; i++) 
+ 	Output: 0 is Prime, 1 is Prime, 2 is Prime, 3 is Prime, 5 is Prime, 7 is Prime, 11 is Prime ... 9973 is Prime
+ 
+ Discussion:
+ 	The program has the correct output only when the outer loop is given integers greater than 1. This is because normally for a number to be lablled as a prime number 
+ 	it needs to be divisble by only two integers. 1, however, is divisble only by one integer, itself, therefore 1 is not labeled as a prime number therefore 
+ 	it should not be inlcuded. However, when the value of 1, or 0, is given to the outerloop the program will determine that they are a prime number. 
+ 	Therefore the lowest value that the outer loop can accept to display correctly is 2. 
+ 	
+ 	Negative numbers are allowed and processed as if legitimate, however, the output is incorrect. This is because 
+ 	by the defintions of prime numbers, primes are integers greater than one with no positive divisors besides one and itself. The program determines that the negative
+ 	integers including 0 and 1 are prime.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	bool chk;
+    
+	for (int i = 2; i < 10000; i++) {
+		chk = true;
+        
+		for (int j = 2; j <= i / 2; j++ ) {
+        
+			if (i % j == 0) {
+				chk = false;
+				break;
+            }
+        }
+        
+		if (chk == true)
+			cout << i << " is Prime" << endl;
+    }
+} ///:~
